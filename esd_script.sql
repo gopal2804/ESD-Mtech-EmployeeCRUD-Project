351e4985ece821d4dd15224bd5e15f7603c5d547
GOPAL GUPTA <51901743+gopal2804@users.noreply.github.com>
2022-12-01 14:29:44 +0530

    Add files via upload

---
+++ esd_script.sql
@@ -0,0 +1,6 @@
+insert into department values(1,1,"HR");
+insert into department values(2,2,"SAC");
+insert into department values(3,5,"Health and care");
+insert into department values(4,7,"Placement");
+insert into employee values(1,"gopalgupta2804@gmail.com","gopal","gupta","xyz","mr",1);
+
